44319515bb14f413ab0db603c37e61f1adff17ca
aramval <annaramosvalldeperez@gmail.com>
2025-02-27 10:12:02 +0100

    Inicio de API yes/no

--- src/components/chat/ChatMensajes.vue
+++ src/components/chat/ChatMensajes.vue
@@ -2,22 +2,22 @@
     
     <div class="p-4 flex-1 overflow-y-auto">
         <div class="flex flex-col space-y-2">
-            <div class="flex justify-end">
-             <div class="bg-blue-200 text-black p-2 max-w-xs rounded-lg">
-                Hola, ¿Que tal el dia ?
-                </div>
-            </div>
-       
-
-        <div class="flex">
-            <div class="bg-gray-200 text-black p-2 max-w-xs rounded-lg">
-               Fatal, grácias.
-            </div>
+            <Burbuja :meus-est="true" nuntius="¿Salimos a cenar?"/>
         </div>
     </div>
-    </div>
+
+    
+          
+        
+  
 
 
 
 
 </template>
+
+<script lang="ts" setup>
+
+import Burbuja from './Burbuja.vue';
+
+</script>
