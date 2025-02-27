80a5bbc1dee00b9b98dda33d88d681b5019e3bd1
aramval <annaramosvalldeperez@gmail.com>
2025-02-27 09:54:40 +0100

    Plantilla de chat

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
-  <h1>Mi primera app de Vue</h1>
-  <MeusNumerator :valorem="5" />
-  <MeusNumeratorScript :valorem="10"/>
-</template>
-<script lang="ts setup">
-import MeusNumerator from "./components/MeusNumerator.vue"
-import MeusNumeratorScript from "./components/MeusNumeratorScript.vue"
+  <IndecisionView/>
+  </template>
 
-</script>
+
+  <script lang="ts" setup>
+  import IndecisionView from './views/IndecisionView.vue';
+  </script>
