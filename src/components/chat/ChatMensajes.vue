f1c87a4ddcd05ebabb0ec2bc3da9230d9644010e
aramval <annaramosvalldeperez@gmail.com>
2025-02-27 10:00:56 +0100

    Componentes de Chat

---
+++ src/components/chat/ChatMensajes.vue
@@ -0,0 +1,23 @@
+<template>
+    
+    <div class="p-4 flex-1 overflow-y-auto">
+        <div class="flex flex-col space-y-2">
+            <div class="flex justify-end">
+             <div class="bg-blue-200 text-black p-2 max-w-xs rounded-lg">
+                Hola, ¿Que tal el dia ?
+                </div>
+            </div>
+       
+
+        <div class="flex">
+            <div class="bg-gray-200 text-black p-2 max-w-xs rounded-lg">
+               Fatal, grácias.
+            </div>
+        </div>
+    </div>
+    </div>
+
+
+
+
+</template>
