c362ebf2ab913b9ad4ef95972418e35f5e326daf
aramval <annaramosvalldeperez@gmail.com>
2025-03-03 17:56:09 +0100

    Chat con git YN

--- src/components/chat/ChatMensajes.vue
+++ src/components/chat/ChatMensajes.vue
@@ -3,6 +3,11 @@
     <div class="p-4 flex-1 overflow-y-auto">
         <div class="flex flex-col space-y-2">
             <Burbuja :meus-est="true" nuntius="¿Salimos a cenar?"/>
+
+            <Burbuja :meus-est="false" 
+            nuntius="no" 
+            imago="https://yesno.wtf/assets/no/17-829284e9dd894ce9fb65fbe86d2e382c.gif"/>
+
         </div>
     </div>
 
